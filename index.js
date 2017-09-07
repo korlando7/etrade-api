977571b5d78658c755115442d9d1cccf0f431719
Kevin <korlando@redventures.com>
2017-09-07 16:03:42 -0400

    add function to get quotes with tokens

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const auth = require('./lib/auth');
 
 const oauthConsumerKey = process.env.OAUTH_CONSUMER_KEY;
 const secretKey = process.env.CONSUMER_SECRET;
+const ticker = process.argv[2];
 
-auth.getTokens(oauthConsumerKey, secretKey).then(data => data);
+
+auth.getTokens(oauthConsumerKey, secretKey)
+  .then(data => data)
+  .then(data => auth.getQuote(data, ticker));
