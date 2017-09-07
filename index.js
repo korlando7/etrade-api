792c4b78330be7ccf70160ece9f75ec2983b83e7
Kevin <korlando@redventures.com>
2017-09-07 13:48:19 -0400

    add async token request

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const auth = require('./lib/auth');
 
+const oauth_consumer_key = process.env.OAUTH_CONSUMER_KEY;
+const secret_key = process.env.CONSUMER_SECRET;
+
+auth.getTokens(oauth_consumer_key, secret_key).then(data => {
+  console.log(data);
+});
